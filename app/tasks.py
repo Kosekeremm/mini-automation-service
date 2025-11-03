26ab1e16bf6b99ff5c0de8967853610ccdcde7c3
Kosekeremm <kerem.1005@outlook.com>
2025-11-03 03:48:16 +0300

    Create tasks.py

---
+++ app/tasks.py
@@ -0,0 +1,20 @@
+from flask_apscheduler import APScheduler
+from flask import current_app
+import time
+
+
+scheduler = APScheduler()
+
+
+
+
+def scheduled_job():
+# Bu görev düzenli aralıklarla çalışır; burada sadece log atılıyor.
+current_app.logger.info("Scheduled job çalıştı - zaman: %s", time.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+
+
+def init_scheduler(app):
+scheduler.init_app(app)
+scheduler.start()
